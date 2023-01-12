b9ec11bf153c6753d00029e596e17005639f5c4c
aLoongQwQ <moe@mc-m.net>
2023-01-12 14:25:43 +0800

    IPHash的检验

--- init.py
+++ init.py
@@ -1,5 +1,9 @@
 import sys
+import time
 import base64
+import hashlib
+
+start = time.time()
 if sys.argv[1] == "":
     print("Usage: vmesspp --version --server")
 if sys.argv[1] != "1.0":
@@ -14,5 +18,20 @@ serverip = bs
 del(bs)
 t            = servermsg[2]
 servermsg[1] = servermsg[2] # IP地址
-servermsg[2] = t[4:]        # 服务器端口
+servermsg[2] = t[:4]        # 服务器端口
+iphash       = t[4:]        # IP hash
 del(t)
+serverip     = servermsg[1] + ':' + servermsg[2]
+end = time.time()
+print("[Log] 服务器地址解析完成,耗时" + str(start-end))
+obj = hashlib.md5()
+obj.update(servermsg[1].encode("utf-8"))
+result=obj.hexdigest()
+print("[Log]解读IPhash:" + result)
+print("[Log]开始检验IPhash")
+if iphash != result:
+    print("[ERROR]IPhash校验失败,建议删除vmess++客户端并删除p,q的值")
+    sys.exit(1)
+print("[Log] IPhash检验成功 开始连接vmess++服务器")
+
+print("[Info] vmess++ server ip:" + serverip)
